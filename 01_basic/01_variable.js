e37e4bea1314561391cb6cb60ba1fb50bc189dee
Prateek Singh <iamprateek22@gmail.com>
2024-05-22 16:01:18 +0000

    variables in js

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 132
+var accountName = "sam"
+let city = "ny"
+state = "uk"
+
+// accountId = 112  not allowed to change const value
+
+/* var is not used now because of block scope and function scope*/ 
+
+accountName = "john"
+city = "pune"
+state = "maharastra"
+
+// let country; gives undefined
+
+console.log(accountId);
+console.table([accountId, accountName, city, state])
+console.log(country)
+
