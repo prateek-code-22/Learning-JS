91c17e6c883f421b10c9c35a7d329d88f4bac98d
Prateek Singh <iamprateek22@gmail.com>
2024-05-28 03:48:22 +0000

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -27,3 +27,47 @@ console.log(typeof strNumber) // string "33"
 // true => 1; false=>0
 // "" => false
 // "hello" => true
+
+//type of conversion:
+// Number
+// String
+// Boolean
+
+
+// *************************** Operations *************************
+
+
+let val = 3
+let negval = -val
+console.log(negval) //-3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "world"
+
+let str3 = str1 + str2
+console.log(str3)  // helloworld
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true); //1
+console.log(+""); //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1, num2, num3);
+
+let count = 100;
+++count;
+console.log(count);
+
