cac46d294ff283181d4591ee313909116afd8386
Prateek Singh <iamprateek22@gmail.com>
2024-05-29 03:04:06 +0000

    data type summary

---
+++ datatypes_summary.js
@@ -0,0 +1,30 @@
+// Primitive data types
+
+// 7 types: String, Number, Boolean, null, undefinedm Symbol, BigInt
+
+const score = 100
+const scoreVal = 100.3
+
+const logged = false
+const temp = null
+let user;
+
+const id = Symbol("123")
+const anotherid = Symbol("123")
+
+const bigNumber = 445654323456543n
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["ironman", "hulk", "black window"]
+let myobj = {
+    name: "prateek",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("hello world!");
+}
+
